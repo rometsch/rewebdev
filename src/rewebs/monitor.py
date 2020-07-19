74bd70067b6743ae411746bbe8dd14733e803439
Thomas Rometsch <thomas.rometsch@gmail.com>
2020-07-19 12:46:49 +0200

    use a filter to exclude hidden directories

--- src/rewebs/monitor.py
+++ src/rewebs/monitor.py
@@ -10,6 +10,8 @@ import asyncore
 
 import pyinotify
 
+import os
+
 mask = pyinotify.IN_DELETE | pyinotify.IN_CLOSE_WRITE
 
 
@@ -24,13 +26,18 @@ class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CLOSE_WRITE(self, event):
         self.function()
 
+def exclude_filter(x):
+    """ A filter to exclude hidden directories. """
+    parts = os.path.normpath(x).lstrip(os.path.sep).split(os.path.sep)
+    rv = any((s[0] == "." for s in parts))
+    return rv
 
 def monitor(directory, callback):
     """ Monitor a directory and call callback on selected events. """
     wm = pyinotify.WatchManager()
     # rec=True, to monitor all sub directories recursively.
-    # auto_add=True, to monitor added new sub directories.
-    wm.add_watch(directory, mask, rec=True, auto_add=True)
+    # auto_add=True, to monitor added new sub directories.        
+    wm.add_watch(directory, mask, rec=True, auto_add=True, exclude_filter=exclude_filter)
     # specify the event handler to process the events.
     pyinotify.AsyncNotifier(wm, EventHandler(callback))
     # start the asyncore loop to monitor and process events.
